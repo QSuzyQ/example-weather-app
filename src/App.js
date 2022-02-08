11bf373e49064894b4769e0032007a6e58f8ab56
QSuzyQ <92366704+QSuzyQ@users.noreply.github.com>
2022-02-08 23:20:42 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project is coded by Suzana Kešerac as taught by Matt Delac on
+        SheCodes Workshop and is{" "}
+        <a href="https://github.com/QSuzyQ/example-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
