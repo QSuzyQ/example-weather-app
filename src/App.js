a7190e0b2c981d5b2ea0e20ea098e2637a766d47
QSuzyQ <92366704+QSuzyQ@users.noreply.github.com>
2022-02-09 00:04:51 +0100

    Added Weather component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        Weather App
+        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project is coded by Suzana Kešerac as taught by Matt Delac on
           SheCodes Workshop and is{" "}
           <a
             href="https://github.com/QSuzyQ/example-weather-app"
             target="_blank"
+            rel="noreferrer"
           >
             open-sourced on GitHub
           </a>
