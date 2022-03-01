41fe86d2de13e88ae50677677eab6dde2b60742f
QSuzyQ <92366704+QSuzyQ@users.noreply.github.com>
2022-03-01 22:23:08 +0100

    Created WeatherForecast component

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -28,7 +28,7 @@ export default function WeatherIcon(props) {
       <ReactAnimatedWeather
         icon="CLEAR_DAY"
         color="black"
-        size={64}
+        size={props.size}
         animate={true}
       />
     );
